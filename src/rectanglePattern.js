a75d6218af9954445d8f39bae52060ef89d68300
Nakka Leelaprasanth <44019459+leelanakka@users.noreply.github.com>
2018-10-27 12:18:42 +0530

    deleted invidual pattern files because already moved those into lib file
    changed the path of the files in the run_all_tests
    all tests passed

--- src/rectanglePattern.js
+++
@@ -1,8 +0,0 @@
-const createRectangle = require('./patternsLib.js').createRectangle;
-const main = function(){
-  let type = process.argv[2];
-  let length = process.argv[3];
-  let breadth = process.argv[4];
-  console.log( createRectangle(type,length,breadth));
-}
-main();
